fb2993a2e71d2b380837b176969c385aa92b7e78
Miziak <mail@miziak.org>
2013-09-04 19:24:09 +0200

    new cmd options parser

---
+++ CmdParser.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import sys, re
+
+
+class CmdParser(object):
+    """Class parsing input cmd options."""
+    def __init__(self, name, usage, version):
+       self.name = name
+       self.usage = usage
+       self.version = version
+       self.values_dict = {}
+       self.options_dict = {}
+       self.options_dict[name] = ("h", "help", "exist", "show help", False)
+
+    def addOpt(self, name, flag, exflag, action, help, default):
+        """addOpt method add new flag to parser"""
+        self.options_dict[name] = (flag, exflag, action, help, default)
+        if(default != None):
+            self.values_dict[name] = default
+
+    def praseCmd(self):
+        """praseCmd method prase input options to output dict"""
+        opt = sys.argv[1:]
+        
+        flag = []
+        while(opt):
+            it = opt[0]
+            if(it[0] == "-" and it[1] != "-" and not flag):
+                for c in it[1:]:
+                    throw = True
+                    for i,v in self.options_dict.iteritems():
+                        if(c == v[0]):
+                            if(v[2] == "exist"):
+                                self.values_dict[i] = True
+                                throw = False
+                            else:
+                                flag.append(i)
+                                throw = False
+
+                            break
+                        elif(c == "h"):
+                            print self.name + " v" + self.version + "\n" + self.usage
+                            print "Options:\n"
+                            for i in self.options_dict.itervalues():
+                                print "\t-" + i[0] + "  --" + i[1] + "\t" + i[3]
+
+                            return
+                        
+                    if(throw):
+                        raise Exception("Unknown option " + it + "!")
+
+                for i in range(0, len(flag)):
+                    self.values_dict[flag[i]] = opt[1:][i]
+
+                opt = opt[len(flag)+1:]
+                flag = []
+            elif(it[0:2] == "--"):
+                m = re.match(r"--(.*)=(.*)", it)
+                for i,v in self.options_dict.iteritems():
+                    if(m.group(1) == v[1]):
+                        self.values_dict[i] = m.group(2)
+                        opt = opt[1:]
+                        break
+                
+                raise Exception("Unknown option --" + m.group(1) + "!")
+            else:
+                raise Exception("Unknown option " + it + "!")
--- __main__.py
+++ __main__.py
@@ -6,48 +6,46 @@ Created on 13-12-2012
 @author: miziak
 '''
 
-import wx, sys, os, re
-from optparse import OptionParser
+import wx, os, re
+#from optparse import OptionParser
 from TrayIcon import *
 from NapiProjekt import *
 from AnimatedText import *
+from CmdParser import *
 
 def main():
-    app = wx.App(redirect=False)
+    parser = CmdParser("NapiTux", "", "0.1beta")
+    parser.addOpt("console", "c", "console", "exist", "change application mode to console", False)
+    parser.addOpt("target", "t", "target", "var", "set downloading target may be directory or file", "")
+    parser.addOpt("language", "l", "lang", "var", "set languago of subtitles(PL or ENG, default PL)", "PL")
+
+    try:
+        parser.praseCmd()
+    except Exception as e:
+        print e
+
+    opt = parser.values_dict
     
-    parser = OptionParser(usage="usage: %prog [options] filename", version="%prog 0.1")
-    parser.add_option("-c", "--console-mode", action="store_true", dest="console", default=False, help="run program only in console mode")
-    parser.add_option("-w", "--without-gui", action="store_true", dest="gui", default=False, help="run program without GUI")
-    parser.add_option("-d", "--directory",  action="store", dest="dir", default="", help="directory witch video files")
-    parser.add_option("-l", "--language",  action="store", dest="lang", default="PL", help="language of subtitles(PL or ENG)")
-    (options, args) = parser.parse_args()
-    option_dict = vars(options)
-    
-    if(option_dict["console"]):
-        napi = NapiProjekt(args[0])
-        if(napi.downloadSubtitles(option_dict["lang"] == "ENG")):
-            print("Pobrano napisy do: " + os.path.basename(args[0]) + "!")
-            downloaded = True
+    if(opt["console"]):
+        if(os.path.isfile(opt["target"])):
+            napi = NapiProjekt(opt["target"])
+            if(napi.downloadSubtitles(opt["language"] == "ENG")):
+                print("Pobrano napisy do: " + os.path.basename(opt["target"]) + "!")
+            else:
+                print("Nie znaleziono napisów do: " + os.path.basename(opt["target"]) + "!")
         else:
-            print("Nie znaleziono napisów do: " + os.path.basename(args[0]) + "!")
-            downloaded = False
-        
-        if(not option_dict["gui"]):
-            napi.getMoreInfo()
-            InfoBalloon(None, napi.info, downloaded).Show()
-    elif(option_dict["dir"]):
-        dir = completePath(option_dict["dir"])
-        for filename in os.listdir(dir):
-            if(re.match(".+\.(avi|mp4|mkv)", filename)):
-                napi = NapiProjekt(dir + filename)
-                if(napi.downloadSubtitles()):
-                    print("Pobrano napisy do: " + os.path.basename(filename) + "!")
-                else:
-                    print("Nie znaleziono napisów do: " + os.path.basename(filename) + "!")
-    else:
-        
-        MainWindow()
-        
+            dir = completePath(opt["target"])
+            for filename in os.listdir(dir):
+                if(re.match(".+\.(avi|mp4|mkv)", filename)):
+                    napi = NapiProjekt(dir + filename)
+                    if(napi.downloadSubtitles(opt["language"] == "ENG")):
+                        print("Pobrano napisy do: " + os.path.basename(filename) + "!")
+                    else:
+                        print("Nie znaleziono napisów do: " + os.path.basename(filename) + "!")
+ 
+ 
+    app = wx.App(redirect=False)
+    MainWindow()    
     app.MainLoop()
     '''app = wx.App(redirect=False)
     frame = MyForm().Show()
