0b9b770bd1ab4714cb422d2738eaff825afd87be
Miziak <mail@miziak.org>
2013-09-12 12:27:19 +0200

    subtitles classes

---
+++ subformats/SubRip.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from Subtitles import *
+
+class SubRip(object):
+    """docstring for SubRip"""
+    def __init__(self, data, info = None):
+        super(SubRip, self).__init__(data, info)
+        
