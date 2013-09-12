e1516daf49f020132d5160f01219e89f20700b7c
Miziak <mail@miziak.org>
2013-09-13 01:07:41 +0200

    some fixes

--- subformats/Subtitles.py
+++ subformats/Subtitles.py
@@ -15,6 +15,7 @@ class Subtitles(object):
     def __init__(self, data, info = None):
         self.info = info
         if(os.path.isfile(data)):
+            self.name = data
             f = open(data, "r")
             self.data = f.read()
             f.close()
@@ -37,36 +38,6 @@ class Subtitles(object):
 
     def save(self):
         """docstring for save"""
-        f = open("test.srt", "w")
+        f = open(self.name[:-4] + ".srt", "w")
         f.write(self.data)
         f.close()
-"""
-    def framateToTime(self, framate_str, fps):
-        docstring for framesToTime
-        sec = int(int(framate_str)/fps)
-        if(sec != 0):
-            tmp = str(datetime.timedelta(seconds=sec)) 
-            
-            if(":" in tmp[:2]):
-                tmp = "0" + tmp[:1] + tmp[1:]
-
-            return tmp + "," + str((int(framate_str)/fps)%sec)[2:5]
-        else:
-            return "00:00:00,000"
-"""
-
-
-"""
-        tmp = ""
-        if(self.type == SubTypesEnum.TYPE_MICRODVD and target == SubTypesEnum.TYPE_SUBRIP):
-            for i,v in enumerate(self.data.splitlines(), start=1):
-                m = re.match(".*\{(\d+)\}\{(\d+)\}(.*)[\r\n]*", v)
-                tmp += str(i) + "\r\n" + self.framateToTime(m.group(1), fps) + " --> " + self.framateToTime(m.group(2), fps) + "\r\n" + m.group(3).replace("|", "\r\n") + "\r\n\r\n"
-                #print self.framateToTime(m.group(1), fps)
-            #print tmp
-
-        self.type = target
-        self.data = tmp
-
-"""
-
